4fe73ce83432d4254cfc67e2ad1822ba258c7999
ZarinSayyed <>
2021-06-08 17:16:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+operator=input("Enter the operation to be done on numbers: ")
+
+if operator == "+":
+  print (num1 + num2)
+elif operator == "*":
+  print(num1 * num2)
+elif operator == "/":
+  print(num1 / num2)
+elif operator == " - ":
+  print(num1 - num2)
+else :
+  print("Enter valid operator")
